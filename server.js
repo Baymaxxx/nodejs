1d12404b1083e1cd00e5defb245f9a32a9b1ee51
baymaxxx <81243232@qq.com>
2016-01-06 13:25:58 +0800

    up

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var http = require('http');
+http.createServer(function(req,res){
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.end('Hello world');
+}).listen(8888);
+console.log('server is running');
